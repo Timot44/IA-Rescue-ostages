7f724756dba5c1a43d85f77f0761ad04d5839cf4
sam <samuel.brb19@gmail.com>
2021-03-12 09:32:26 +0100

    GameManager & Weapon Empty

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> itemSpawnable;
+
+    public List<Transform> placeToSpawnItems;
+
+    public Transform currentRespawnPoint;
+    private float _timerToSpawnItem;
+
+    public bool isPhaseTwo;
+
+    #region singleton
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        { Instance = this;
+        }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SwitchPhaseForAll()
+    {
+    }
+
+    private void SpawnItem()
+    {
+    }
+}
--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Weapons : MonoBehaviour
+[CreateAssetMenu(fileName = "Weapon", menuName = "Scriptable/Weapons")]
+public class Weapons : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string name;
+    public int startAmmo;
+    public int fireRate;
+    public int damage;
 }
