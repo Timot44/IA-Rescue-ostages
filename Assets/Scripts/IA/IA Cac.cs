f66432df1800ab40cb7c905e246a9e54e23521b7
Timothé <rondeautimothe@gmail.com>
2021-03-12 11:45:20 +0100

    IA Cac_empty

---
+++ Assets/Scripts/IA/IA Cac.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IACac : IAParent
+{
+    public float attackDistance;
+    public float speedBoost;
+    public int damageBoost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    
+}
