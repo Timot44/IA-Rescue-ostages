fc8d8eef8f21985cb300fb9fa8b5a975b0346214
Timothé <rondeautimothe@gmail.com>
2021-03-18 10:23:07 +0100

    SimpleGun_resoBug

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -17,11 +17,11 @@ public class Gun : MonoBehaviour
      
       if (other.gameObject.GetComponent<PlayerShoot>())
       {
-         //On set l'arme du joueur avec cette arme et on la tp au niveau du placeHolder, on setUp aussi les paramètres
+         //On set l'arme du joueur avec cette arme et on la tp au niveau du placeHolder, on setUp aussi les paramètres de l'arme
          other.gameObject.GetComponent<PlayerShoot>().currentWeapoons = weaponToAdd;
          gameObject.transform.SetParent(other.gameObject.GetComponent<PlayerShoot>().weapons_holders);
          other.gameObject.GetComponent<PlayerShoot>().SetUpCurrentWeapon();
-         
+         //On set up la position et la rotation de l'arme par rapport au placeHolder
          gameObject.transform.position = other.gameObject.GetComponent<PlayerShoot>().weapons_holders.position;
          gameObject.transform.rotation = other.gameObject.GetComponent<PlayerShoot>().weapons_holders.rotation;
          
