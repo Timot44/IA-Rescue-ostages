702c32b3c8cba8585b633cef6f194412544b2ef8
Milo <florentin.roberge@gmail.com>
2021-03-26 10:55:20 +0100

    Add simple hostage behavior

---
+++ Assets/PlayerNearDetection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNearDetection : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponentInParent<IAHostage>().SetStateToPhaseTwo();
+    }
+}
--- Assets/Scripts/IA/IAHostage.cs
+++ Assets/Scripts/IA/IAHostage.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IAHostage : MonoBehaviour
 {
-    public int health;
-    private int maxHealth;
+    private int health;
+    public int maxHealth = 100;
+    public float correctDistanceFromPlayer = 2f;
 
-    public void SwitchToState(int stateIndex)
-    {
-        
-    }
+    private bool fleeingEnemies;
+    private bool phaseTwo;
+    private NavMeshAgent agent;
+    private Transform player;
 
     public void TakeDamage(int damage)
     {
@@ -23,11 +26,26 @@ public class IAHostage : MonoBehaviour
 
     public void SetStateToPhaseTwo()
     {
-        
+        phaseTwo = true;
     }
 
     public void Dead()
     {
         
+    }
+
+    public void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    public void Update()
+    {
+        if (phaseTwo)
+        {
+            if (Vector3.Distance(player.position, transform.position) > correctDistanceFromPlayer)
+            {
+            }
+        }
     }
 }
