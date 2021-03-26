7d01f5dd27be5ac229cf075e24c822c04a993407
Timothé <rondeautimothe@gmail.com>
2021-03-26 10:51:07 +0100

    push_IACAC_début

--- Assets/Scripts/IA/IA Cac/CacState.cs
+++ Assets/Scripts/IA/IA Cac/CacState.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CacState 
+public abstract class CacState
 {
+   protected int _index;
    public virtual void Move(IACac ctx)
    {
-      
+      if (!ctx.isPlayerDetected)
+      {
+         ctx.agent.SetDestination(ctx.patrolWaypoint[_index].position);
+            
+         if (Vector3.Distance(ctx.gameObject.transform.position, ctx.patrolWaypoint[_index].position) <=
+             ctx.distanceToChangeWaypoint)
+         {
+            _index++;
+         }
+
+         if (_index >= ctx.patrolWaypoint.Length)
+         {
+            _index = 0;
+         }
+      }
+      else
+      {
+         Vector3 dirToPlayer = (ctx.transform.position - ctx.player.position).normalized;
+         Vector3 newPosTarget = ctx.transform.position + (dirToPlayer * 25);
+
+         ctx.agent.SetDestination(newPosTarget);
+      }
    }
 
    public virtual void Attack(IACac ctx)
@@ -16,7 +38,14 @@ public abstract class CacState
 
    public virtual void RunToPlayer(IACac ctx)
    {
-      
+      if (ctx.isPlayerDetected)
+      {
+         if (ctx)
+         {
+            
+         }
+         ctx.agent.SetDestination(ctx.player.position);
+      }
    }
 
 }
---
+++ Assets/Scripts/IA/IA Cac/IACac.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IACac : IAParent
+{
+    public float attackDistance;
+    public float speedBoost;
+    public int damageBoost;
+    public bool isPlayerDetected;
+    public CacState currentState;
+    
+    
+    public NavMeshAgent agent;
+    public Transform player;
+    // Start is called before the first frame update
+    public override void SwitchToState(int stateIndex)
+    {
+        switch (stateIndex)
+        {
+            case 1:
+                currentState = new CacStateP1();
+                break;
+            case 2:
+                currentState = new CacStateP2();
+                break;
+            default:
+                Debug.LogError("State index do not exist : " + stateIndex);
+                break;
+        }
+    }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentState = new CacStateP1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (IsPlayerDetected())
+        {
+            currentState.RunToPlayer(this);
+            
+        }
+        else
+        {
+            currentState.Move(this);
+            
+        }
+    }
+
+    public bool IsPlayerDetected()
+    {
+        
+        Vector3 iAPos = transform.position;
+        Vector3 iAForward = transform.forward * detectionRange;
+        Vector3 iARight = transform.right * 3f;
+        Ray ray1 = new Ray(iAPos, iAForward);
+        Ray ray2 = new Ray(iAPos,iAForward - iARight);
+        Ray ray3 = new Ray(iAPos,iAForward + iARight);
+        Ray[] rays = new[] {ray1, ray2, ray3};
+        RaycastHit hit;
+        foreach (var ray in rays)
+        {
+            if (Physics.Raycast(ray, out hit, detectionRange))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    Debug.Log(hit.collider.name);
+                    isPlayerDetected = true;
+                    return true;
+                }
+            }
+        }
+        isPlayerDetected = false;
+        return false;
+        
+        
+    }
+    
+    
+    private void OnDrawGizmosSelected()
+    {
+        Debug.DrawRay(transform.position,detectionRange * transform.forward, Color.red);
+        Debug.DrawRay(transform.position,detectionRange * transform.forward - transform.right * 3, Color.blue);
+        Debug.DrawRay(transform.position,detectionRange * transform.forward + transform.right * 3, Color.green);
+    }
+    
+}
