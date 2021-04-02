c0e20371c3aec8fa8acc365298d58ae43e842498
Milo <florentin.roberge@gmail.com>
2021-04-02 13:27:33 +0200

    Update Comment

--- Assets/Scripts/IA/IAHostage.cs
+++ Assets/Scripts/IA/IAHostage.cs
@@ -25,18 +25,19 @@ public class IAHostage : MonoBehaviour
     public float atSaveSpotDistance = 1f;
     public float checkIfIsAttackedTime = 1f;
 
-    private bool fleeingEnemies;
-    private bool phaseTwo;
-    private float saveSpotTimer = 1f;
+    private bool _fleeingEnemies;
+    private bool _phaseTwo;
+    private float _saveSpotTimer = 1f;
         
-    private Vector3 saveSpotPos;
-    private Transform player;
-    private IAParent[] enemies;
+    private Vector3 _saveSpotPos;
+    private Transform _player;
+    private IAParent[] _enemies;
     
     
 
     public void TakeDamage(int damage)
     {
+        // Handle damage and death
         health -= damage;
         if (health <= 0)
         {
@@ -46,9 +47,11 @@ public class IAHostage : MonoBehaviour
 
     public void SetStateToPhaseTwo()
     {
-        if (!phaseTwo)
+        // If the state is not already in phase two, send the information to the game manager to start phase two and
+        // start invoke repeating to check if the AI is attacked
+        if (!_phaseTwo)
         {
-            phaseTwo = true;
+            _phaseTwo = true;
             GameManager.Instance.SwitchPhaseForAll();
             InvokeRepeating("IsAttacked", 0, checkIfIsAttackedTime);
         }
@@ -56,26 +59,32 @@ public class IAHostage : MonoBehaviour
 
     public void Dead()
     {
-        GameManager.Instance.RespawnPlayer(player.gameObject, gameObject);
+        // If the AI is dead, start Respawn process
+        GameManager.Instance.RespawnPlayer(_player.gameObject, gameObject);
     }
 
     public void Start()
     {
+        // Initialize variables
         health = maxHealth;
-        player = FindObjectOfType<Player>().transform;
-        enemies = FindObjectsOfType<IAParent>();
+        _player = FindObjectOfType<Player>().transform;
+        _enemies = FindObjectsOfType<IAParent>();
     }
 
     public void Update()
     {
+        // Set slider for life to show health
         healthBarSlider.value = health;
         
-        if (phaseTwo)
+        // If phase two as started the AI as a behavior, if not he do nothing
+        if (_phaseTwo)
         {
-            if (!fleeingEnemies)
-                if (Vector3.Distance(player.position, transform.position) > correctDistanceFromPlayer)
+            if (!_fleeingEnemies)
+                // If he does not flee the ennemie he simply follow the player
+                //Stop following if it's to close. 
+                if (Vector3.Distance(_player.position, transform.position) > correctDistanceFromPlayer)
                 {
-                    agent.destination = player.transform.position;
+                    agent.destination = _player.transform.position;
                 }
                 else
                 {
@@ -83,38 +92,43 @@ public class IAHostage : MonoBehaviour
                 }
             else
             {
-                
-                if (saveSpotPos == null || saveSpotPos == Vector3.zero)
+                // Check is the savespot has been initialize or have a value of zero to start searching
+                if (_saveSpotPos == null || _saveSpotPos == Vector3.zero)
                 {
-                    saveSpotPos = SearchSaveSpot();
+                    _saveSpotPos = SearchSaveSpot();
                 }
 
-                if (saveSpotPos != Vector3.zero && AtSaveSpot() && saveSpotTimer <= 0)
+                // Recheck if the AI is at save point and that it need to check for another one (not 100% of the time for optimisation purpose)
+                if (_saveSpotPos != Vector3.zero && AtSaveSpot() && _saveSpotTimer <= 0)
                 {
+                    // Recheck if enemies are no longer attacking
                     List<Transform> enemyTransforms = new List<Transform>();
-                    for (int i = 0; i < enemies.Length; i++)
+                    for (int i = 0; i < _enemies.Length; i++)
                     {
-                        if (enemies[i] != null)
-                            if (enemies[i].isAttack && enemies[i])
-                                enemyTransforms.Add(enemies[i].transform);
+                        if (_enemies[i] != null)
+                            if (_enemies[i].isAttack && _enemies[i])
+                                enemyTransforms.Add(_enemies[i].transform);
                     }
 
+                    // Check if the point is not still safe, search for another point
                     if (!CheckPointSafe(transform.position, enemyTransforms))
                     {
                         SearchSaveSpot();
                     }
 
-                    saveSpotTimer = 1f;
+                    // Restart the timer for the next search
+                    _saveSpotTimer = 1f;
                 }
                 else
                 {
-                    saveSpotTimer -= Time.deltaTime;
+                    // Decrease the timer before the next search
+                    _saveSpotTimer -= Time.deltaTime;
                 }
 
-                if (saveSpotPos != Vector3.zero)
+                // If the save point is init, if it is, go to savepoint
+                if (_saveSpotPos != Vector3.zero)
                 {
-                    Debug.Log("GotoSavespot : " + saveSpotPos);
-                    agent.destination = saveSpotPos;
+                    agent.destination = _saveSpotPos;
                 }
             }
         }
@@ -122,22 +136,24 @@ public class IAHostage : MonoBehaviour
 
     private void IsAttacked()
     {
-        foreach (var ias in enemies)
+        // Check every enemy to check if one of them is attacking
+        foreach (var ias in _enemies)
         {
             if (ias != null)
                 if (ias.isAttack)
                 {
-                    fleeingEnemies = true;
+                    _fleeingEnemies = true;
                     return;
                 }
         }
 
-        fleeingEnemies = false;
+        _fleeingEnemies = false;
     }
 
     private bool AtSaveSpot()
     {
-        if (Vector3.Distance(saveSpotPos, transform.position) <= atSaveSpotDistance)
+        // Check if the AI is at the save position or if it need to continue
+        if (Vector3.Distance(_saveSpotPos, transform.position) <= atSaveSpotDistance)
             return true;
         else
             return false;
@@ -145,32 +161,41 @@ public class IAHostage : MonoBehaviour
 
     private Vector3 SearchSaveSpot()
     {
+        // Reset save spot to a non correct value
         Vector3 saveSpot = Vector3.zero;
 
         // Get all enemies transform
         List<Transform> enemyTransforms = new List<Transform>();
-        for (int i = 0; i < enemies.Length; i++)
+        for (int i = 0; i < _enemies.Length; i++)
         {
-            if (enemies[i] != null)
-                if (enemies[i].isAttack)
-                    enemyTransforms.Add(enemies[i].transform);
+            if (_enemies[i] != null)
+                if (_enemies[i].isAttack)
+                    enemyTransforms.Add(_enemies[i].transform);
         }
 
+        // For each circle
         for (int i = 0; i < numberOfCirclesToCheck; i++)
         {
+            // Calculate the radius of the circle
             float circleDistance = (i + 1) * distanceToCheck / numberOfCirclesToCheck;
+            // Re-calculate the number of point with percentage optimisation
             int trueNumberOfPoint = Mathf.RoundToInt(numberOfPointPerCircles *
                                                      Mathf.Pow(1 - percentDecreasePerCircle,
                                                          numberOfCirclesToCheck - i - 1));
+            // Calculate how much degree we need to add per points
             float degreePerPoint = 360 / trueNumberOfPoint;
 
+            // For each point on this circle
             for (int y = 0; y < trueNumberOfPoint; y++)
             {
+                // Calculate point position
                 Vector3 pointPosition = new Vector3(circleDistance * Mathf.Sin(Mathf.Deg2Rad * degreePerPoint * y), 0,
-                    circleDistance * Mathf.Cos(Mathf.Deg2Rad * degreePerPoint * y)) + transform.position;
+                                            circleDistance * Mathf.Cos(Mathf.Deg2Rad * degreePerPoint * y)) + transform.position;
 
+                // Check if the point is safe
                 if (CheckPointSafe(pointPosition, enemyTransforms))
                 {
+                    // If its safe, the position is validate as a save spot
                     saveSpot = pointPosition;
                     
                     return saveSpot;
@@ -185,7 +210,7 @@ public class IAHostage : MonoBehaviour
 
     private bool CheckPointSafe(Vector3 pointPos, List<Transform> enemyTransforms)
     {
-        // Check is
+        // Check for every hostile ennemies if the distance of the point is correct
         for(int i = 0; i < enemyTransforms.Count; i++)
         {
             if (Vector3.Distance(enemyTransforms[i].position, pointPos) < minDistanceFromEnemies)
@@ -195,10 +220,11 @@ public class IAHostage : MonoBehaviour
                 
         }
 
+        // Check if the point is not in a wall
         RaycastHit hitSphere;
         if (!Physics.SphereCast(pointPos, .2f, Vector3.down, out hitSphere, .2f, obstaclesLayers))
         {
-            
+            // Check foreach ennemy if he has an obstacle between the point and them, if their is one, the point is safe.
             foreach (var enemy in enemyTransforms)
             {
                 RaycastHit hit;
@@ -210,7 +236,6 @@ public class IAHostage : MonoBehaviour
                 {
                     return false;
                 }
-                    
             }
         }
         else
@@ -218,12 +243,13 @@ public class IAHostage : MonoBehaviour
             return false;
         }
 
-        Debug.Log("PAS NORMAL");
-        return true;
+        Debug.LogError("Fail in CheckPointSafe");
+        return false;
     }
 
     private void OnDrawGizmosSelected()
     {
+        // Draw the circles for detections
         for (int i = 0; i < numberOfCirclesToCheck; i++)
         {
             float circleDistance = (i + 1) * distanceToCheck / numberOfCirclesToCheck;
@@ -239,10 +265,10 @@ public class IAHostage : MonoBehaviour
             }
         }
 
-        if (saveSpotPos != null)
+        if (_saveSpotPos != null)
         {
             Gizmos.color = Color.magenta;
-            Gizmos.DrawSphere(saveSpotPos, .3f);
+            Gizmos.DrawSphere(_saveSpotPos, .3f);
         }
     }
 }
